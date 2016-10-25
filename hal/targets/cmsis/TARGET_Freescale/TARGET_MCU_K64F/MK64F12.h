dc5aaf5990756c95c2cb9636d5232fd0e7f6088b
Christopher Haster <chaster@utexas.edu>
2016-10-25 17:38:22 -0500

    Fixed conditional checks for ARMC6

--- hal/targets/cmsis/TARGET_Freescale/TARGET_MCU_K64F/MK64F12.h
+++ hal/targets/cmsis/TARGET_Freescale/TARGET_MCU_K64F/MK64F12.h
@@ -405,7 +405,7 @@ typedef enum _dma_request_source
 ** Start of section using anonymous unions
 */
 
-#if defined(__ARMCC_VERSION)
+#if defined(__CC_ARM)
   #pragma push
   #pragma anon_unions
 #elif defined(__CWCC__)
@@ -12613,7 +12613,7 @@ typedef struct {
 ** End of section using anonymous unions
 */
 
-#if defined(__ARMCC_VERSION)
+#if defined(__CC_ARM)
   #pragma pop
 #elif defined(__CWCC__)
   #pragma pop
